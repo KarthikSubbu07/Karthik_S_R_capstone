d748f6125028bfbd0eb286484c92ae098ad091c2
Subramanian Raman <k.subramanianraman@elsevier.com>
2026-09-10 13:01:25 +0530

    removed unwanted files

--- sample1.py
+++
@@ -1,5 +0,0 @@
-def add(a, b):
-    return a + b
-
-def greet(name):
-    print(f"Hello, {name}!")
--- sample2.py
+++
@@ -1,6 +0,0 @@
-from sample1 import add, greet
-
-result = add(10, 20)
-print(result)
-
-greet("Person")
